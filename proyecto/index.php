249181055b3532bba85722d0ac8851c16d008d9e
monchoramon <cruzcruzram@hotmail.com>
2020-10-19 00:27:37 -0500

    Proyecto Final Curso Bootstrap

--- proyecto/index.php
+++ proyecto/index.php
@@ -8,9 +8,9 @@
 	<link rel="stylesheet" type="text/css" href="css/lib/fontawesome-all.css">
 	<link rel="stylesheet" href="css/style.css">
 </head>
-<body>
+<body data-spy="scroll" data-target="#main-navbar">
 	
-	<div class="navbar navbar expand-lg navbar-dark bg-dark fixed-top">
+	<div class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="main-navbar">
 		<div class="container">
 			<a href="#" class="navbar-brand text-info font-weight-bold">Logo</a>
 			<button 
@@ -30,16 +30,16 @@
 						<a href="#home" class="nav-link">Home</a>
 					</li>
 					<li class="nav-item">
-						<a href="#Feaure" class="nav-link">Feaure</a>
+						<a href="#feature" class="nav-link">Feaures</a>
 					</li>
 					<li class="nav-item">
-						<a href="#Preview" class="nav-link">Preview</a>
+						<a href="#preview" class="nav-link">Preview</a>
 					</li>
 					<li class="nav-item">
-						<a href="#Details" class="nav-link">Details</a>
+						<a href="#details" class="nav-link">Details</a>
 					</li>
 					<li class="nav-item">
-						<a href="#Contact" class="nav-link">Contact</a>
+						<a href="#contact" class="nav-link">Contact</a>
 					</li>
 				</ul>
 			</div>
@@ -439,12 +439,331 @@
 						</div>
 					</div>
 				</div>
+
+				<!-- Goals Tab -->
+				<div 
+				class="tab-pane fade"
+				id="nav-goals"
+				role="tabpanel"
+				aria-labelledby="nav-goals-tab">
+					<div class="row">
+						<div class="col-lg-8">
+							<div class="box-settings w-100 h-100 d-flex align-items-center justify-content-center mb-3">
+								<div class="row">
+									<div class="col-md-6">
+										<div class="box-icon d-flex align-items-center justify-content-center">
+											<div class="card-icon rounded-circle text-center border border-info">
+												<i class="fas fa-angle-double-left p-3 text-info"></i>
+											</div>
+										</div>
+										<div class="card-body text-center">
+											<h5 class="card-title">Card Title</h5>
+											<p class="card-text">
+												Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa consectetur ad sit repudiandae harum impedit eum saepe deserunt! Qui sed nam nemo ipsa minima tempore autem totam dolore debitis? Atque!
+											</p>
+										</div>
+									</div>
+									<div class="col-md-6">
+										<div class="box-icon d-flex align-items-center justify-content-center">
+											<div class="card-icon rounded-circle text-center border border-info">
+												<i class="fas fa-angle-double-left p-3 text-info"></i>
+											</div>
+										</div>
+										<div class="card-body text-center">
+											<h5 class="card-title">Card Title</h5>
+											<p class="card-text">
+												Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa consectetur ad sit repudiandae harum impedit eum saepe deserunt! Qui sed nam nemo ipsa minima tempore autem totam dolore debitis? Atque!
+											</p>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-md-6">
+											<div class="box-icon d-flex align-items-center justify-content-center">
+												<div class="card-icon rounded-circle text-center border border-info">
+													<i class="fas fa-angle-double-left p-3 text-info"></i>
+												</div>
+											</div>
+											<div class="card-body text-center">
+												<h5 class="card-title">Card Title</h5>
+												<p class="card-text">
+													Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa consectetur ad sit repudiandae harum impedit eum saepe deserunt! Qui sed nam nemo ipsa minima tempore autem totam dolore debitis? Atque!
+												</p>
+											</div>
+										</div>
+										<div class="col-md-6">
+											<div class="box-icon d-flex align-items-center justify-content-center">
+												<div class="card-icon rounded-circle text-center border border-info">
+													<i class="fas fa-angle-double-left p-3 text-info"></i>
+												</div>
+											</div>
+											<div class="card-body text-center">
+												<h5 class="card-title">Card Title</h5>
+												<p class="card-text">
+													Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa consectetur ad sit repudiandae harum impedit eum saepe deserunt! Qui sed nam nemo ipsa minima tempore autem totam dolore debitis? Atque!
+												</p>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4">
+							<div class="box-image w-100 h-100 d-flex justify-content-center align-items-center">
+								<img src="./images/section-nav/goals.png" alt="" class="img-fluid">
+							</div>
+						</div>
+					</div>
+				</div>
+
 			</div> <!-- tab-content -->
 		</div>
+	</section>
+	
+	<section class="section-preview text-white" id="preview">
+		<div class="container">
+			<div class="row">
+				<div class="col-12 text-center text-white">
+					<h2 class="heading-section">Preview</h2>
+					<p class="lead">Whatch the video for more interesting options</p>
+					<hr>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-12">
+					<div class="box-image video text-center">
+						<a
+						class="d-none d-md-block"
+						data-toggle="modal"
+						data-target="#previewModal">
+							<div class="image-boton-preview-box">
+								<img src="./images/section-preview/section-preview-button.jpg" alt="" class="img-fluid">
+							</div>
+						</a>
+						<a 
+						href="https://www.youtube.com/watch?v=lj6CEc-ppqo&list=RDlj6CEc-ppqo&start_radio=1&ab_channel=PostMaloneVEVO" 
+						class="d-md-none"
+						target="_blank">
+							<div class="image-boton-preview-box">
+								<img src="./images/section-preview/section-preview-button.jpg" alt="" class="img-fluid">
+							</div>
+						</a>
+					</div>
+					<div 
+					class="modal fade" 
+					id="previewModal" 
+					tabindex="-1" 
+					role="dialog" 
+					aria-labelledby="previewModalLabel"
+					aria-hidden="true"
+					>
+						<div class="modal-dialog modal-dialog-centered" role="document">
+							<div class="modal-content">
+								<div class="modal-body p-0">
+									<div class="ifram-youtube-box d-flex">
+										<iframe 
+										width="560" 
+										height="315" 
+										src="https://www.youtube.com/embed/lj6CEc-ppqo" 
+										frameborder="0" 
+										allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+										allowfullscreen></iframe>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section
+	class="section-details"
+	id="details">
+		<div class="box-details">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6">
+						<img src="./images/section-details/section-details-1.png" alt="" class="img-fluid">
+					</div>
+					<div class="col-lg-6 d-flex align-items-center">
+						<div class="card bg-transparent border-0 text-white">
+							<div class="card-body">
+								<h1 class="card-title">
+									Lorem ipsum dolor, sit amet consectetur.
+								</h1>
+								<p class="card-text">
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium in optio dolores hic nemo minima dicta autem, aliquam tempore aut! Suscipit sint quia optio! Est sint earum error ratione voluptate.
+								</p>
+								<a href="#" class="btn btn-info">Read More</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="box-details">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6 d-flex align-items-center">
+						<div class="card bg-transparent border-0 text-white">
+							<div class="card-body">
+								<h1 class="card-title">
+									Lorem ipsum dolor, sit amet consectetur.
+								</h1>
+								<p class="card-text">
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium in optio dolores hic nemo minima dicta autem, aliquam tempore aut! Suscipit sint quia optio! Est sint earum error ratione voluptate.
+								</p>
+								<a href="#" class="btn btn-info">Read More</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6">
+						<img src="./images/section-details/section-details-2.png" alt="" class="img-fluid">
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section
+	class="section-contact-form"
+	id="contact">
+		<div class="container">
+			<div class="text-center">
+				<h2 class="heading-section">Contact</h2>
+				<p class="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae ea a corrupti assumenda similique exercitationem porro tempore alias, accusamus numquam quisquam doloremque blanditiis, vero explicabo nulla error, aliquid expedita! Assumenda?</p>
+				<hr>
+			</div>
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="box-contact-info d-flex h-100 w-100 justify-content-center align items-center">
+						 <ul class="list-unstyled">
+							 <li>
+								<i class="fas fa-map-marker-alt text-info"></i>
+								<span>Palo Alto, California US</span>
+							</li>
+							<li>
+								<i class="fas fa-phone text-info"></i>
+								<span>0123456789</span>
+							</li>
+							<li>
+								<i class="fas fa-envelope text-info"></i>
+								<span><a href="mailto:info@exmple.com" class="text-white">info@exmple.com</a></span>
+							</li>
+						 </ul>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<div class="box-fone">
+						<form>
+							<div class="form-group">
+								<input 
+								type="text" 
+								name="name" 
+								id="name-field"
+								class="form-control contact-form-field"
+								required>
+								<small id="nameHelp" class="form-text text-muted">Lorem ipsum dolor sit.</small>
+							</div>
+							<div class="form-group">
+								<input 
+								type="email" 
+								name="email" 
+								id="email-field"
+								class="form-control contact-form-field"
+								required>
+								<small id="comentHelp" class="form-text text-muted">Lorem ipsum dolor sit.</small>
+							</div>
+							<div class="form-group">
+								<textarea 
+								name="comment" 
+								id="comment-field"
+								class="form-control contact-form-field"
+								rows="3"
+								required></textarea>
+								<small id="comentHelp" class="form-text text-muted">Lorem ipsum dolor sit.</small>
+							</div>
+							<button class="btn btn-outline-info btn-lg btn-block">Enviar</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<footer class="py-5 text-center">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4">
+					<div class="box-footer">
+						<h4 class="text-white">Social Media</h4>
+						<div class="box-icons d-flex justify-content-around p-4">
+							<div class="icon-footer rounded-circle text-center border border-info">
+								<a href="#"><i class="fab fa-twitter text-info p-3"></i></a>
+							</div>
+							<div class="icon-footer rounded-circle text-center border border-info">
+								<a href="#"><i class="fab fa-facebook text-info p-3"></i></a>
+							</div>
+							<div class="icon-footer rounded-circle text-center border border-info">
+								<a href="#"><i class="fab fa-instagram text-info p-3"></i></a>
+							</div>
+							<div class="icon-footer rounded-circle text-center border border-info">
+								<a href="#"><i class="fab fa-linkedin text-info p-3"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4">
+					<div class="box-footer">
+						<h4>Map</h4>
+						<div class="box-map p-4">
+							<iframe 
+							src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15051.467345658013!2d-102.01661440000001!3d19.41815875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2smx!4v1603082221810!5m2!1ses!2smx" 
+							width="290" 
+							height="240" 
+							frameborder="0" 
+							style="border:0;" 
+							allowfullscreen="" 
+							aria-hidden="false" 
+							tabindex="0"></iframe>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4">
+					<div class="box-footer text-white">
+						<h4>About Us</h4>
+						<div class="box-about-us p-4">
+							<p class="text-muted">
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. In magnam alias possimus quibusdam corrupti, sapiente qui. Ab earum incidunt tempora quasi harum dolorum voluptatem molestiae! Alias odit porro placeat aliquam.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+	<section class="section-copyright">
+		<p class="text-white text-center p-1 m-0">
+			Copyright &copy; Nuestra WEB <?=date("Y")?>
+		</p>
 	</section>
 	
 	<script type="text/javascript" src="js/lib/jquery-3.4.1.min.js"></script>
 	<script type="text/javascript" src="js/lib/bootstrap.min.js"></script>
 	<script type="text/javascript" src="js/lib/popper.min.js"></script>
+	<script>
+		$(document).ready(function(){
+			$(window).scroll(function(){
+				var scroll = $(window).scrollTop();
+				if ( scroll < 200 ) {
+					$(".fixed-top").removeClass("background-scroll-down");
+				} else {
+					$(".fixed-top").addClass("background-scroll-down");
+				}
+			})
+		})
+	</script>
 </body>
 </html>
